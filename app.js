e3cc006999190698cf87c69243e3eb04549f24e0
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-05-22 18:51:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+});
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
